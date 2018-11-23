6876b575ec184c27faeddb3549fda9e8d51d33a4
Saras Windecker <swindecker@9770L-134432-M.local>
2018-11-23 23:20:49 +1100

    revised install.R

--- install.R
+++ install.R
@@ -1,1 +1,2 @@
-install.packages(c('ggplot2', 'checkmate'))
+purrr::walk2(.x = c("rmarkdown", "ggplot2", "knitr", "checkmate"), .y = c("1.10", "3.1.0", "1.20", "1.8.5"), ~devtools::install_version(package = .x, version = .y))
+# devtools::install_github(c(""))
